5c00cf273e353dc88d8b18b59abd69d440af0655
angela4321 <angelaqiao621@gmail.com>
2020-09-13 02:10:45 -0700

    Basic GUI

---
+++ GUI.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+root = Tk()
+label1 = Label(root,text="Title")
+label1.grid(row=0,column=0)
+entry1 = Entry(root,width=20)
+entry1.grid(row=1,column=0)
+
+label2 = Label(root,text="Artist")
+label2.grid(row=0,column=1)
+entry2 = Entry(root,width=20)
+entry2.grid(row=1,column=1)
+
+label3 = Label(root,text="Genre")
+label3.grid(row=0,column=2)
+entry3 = Entry(root,width=20)
+entry3.grid(row=1,column=2)
+
+label4 = Label(root,text="Listens")
+label4.grid(row=0,column=3)
+entry4 = Entry(root,width=20)
+entry4.grid(row=1,column=3)
+
+label5 = Label(root,text="Likes")
+label5.grid(row=0,column=4)
+entry5 = Entry(root,width=20)
+entry5.grid(row=1,column=4)
+
+def submit():
+    print("here")
+
+submit = Button(root,text="Submit", command = submit)
+submit.grid(row=2,column=2)
+root.mainloop()
